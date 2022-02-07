0871a2a94af79591f2b1b190ada901e9643c2ed4
kim <wlsdiqkdrk@naver.com>
2022-02-07 15:38:36 +0900

    revised

--- back/config/config.js
+++ back/config/config.js
@@ -4,21 +4,21 @@ dotenv.config();
 
 module.exports = {
   "development": {
-    "username": "postgres1",
+    "username": "postgres",
     "password": process.env.DB_PASSWORD,
     "database": "postgres1",
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
   "test": {
-    "username": "postgres1",
+    "username": "postgres",
     "password": process.env.DB_PASSWORD,
     "database": "postgres1",
     "host": "127.0.0.1",
     "dialect": "postgres1"
   },
   "production": {
-    "username": "postgres1",
+    "username": "postgres",
     "password": process.env.DB_PASSWORD,
     "database": "postgres1",
     "host": "127.0.0.1",
