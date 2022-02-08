9b96babaf75f6d1a5a178976662206209ffd0311
kim <wlsdiqkdrk@naver.com>
2022-02-08 10:18:02 +0900

    revised

--- front/src/config/config.js
+++ front/src/config/config.js
@@ -1,3 +1,4 @@
 // export const backUrl = "http://3.35.176.162";
-export const backUrl = "http://api.nodebird.com";
+export const backUrl = "http://15.164.121.12";
+// export const backUrl = "http://api.nodebird.com";
 // export const backUrl = "http://localhost:4001";
